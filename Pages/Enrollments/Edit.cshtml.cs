41dcf2c6c2e26272fae24c8e2a93297e6935fad2
DELL <DELL@DESKTOP-NI5LJRK>
2026-01-27 20:52:50 +0545

    Enrollment

---
+++ Pages/Enrollments/Edit.cshtml.cs
@@ -0,0 +1,96 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OnlineCourseManagementPortal.Data;
+using OnlineCourseManagementPortal.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineCourseManagementPortal.Pages.Enrollments
+{
+	public class EditModel : PageModel
+	{
+		private readonly ApplicationDbContext _context;
+
+		public EditModel(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		[BindProperty]
+		public Enrollment Enrollment { get; set; }
+
+		public SelectList Students { get; set; }
+		public SelectList Courses { get; set; }
+
+		public async Task<IActionResult> OnGetAsync(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			Enrollment = await _context.Enrollments.FirstOrDefaultAsync(m => m.Id == id);
+
+			if (Enrollment == null)
+			{
+				return NotFound();
+			}
+
+			await LoadSelectLists();
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				await LoadSelectLists();
+				return Page();
+			}
+
+			_context.Attach(Enrollment).State = EntityState.Modified;
+
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!EnrollmentExists(Enrollment.Id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			TempData["SuccessMessage"] = "Enrollment updated successfully!";
+			return RedirectToPage("./Index");
+		}
+
+		private bool EnrollmentExists(int id)
+		{
+			return _context.Enrollments.Any(e => e.Id == id);
+		}
+
+		private async Task LoadSelectLists()
+		{
+			var students = await _context.Students
+				.Where(s => s.IsActive)
+				.OrderBy(s => s.LastName)
+				.ToListAsync();
+
+			var courses = await _context.Courses
+				.Where(c => c.IsActive)
+				.OrderBy(c => c.Title)
+				.ToListAsync();
+
+			Students = new SelectList(students, "Id", "FullName");
+			Courses = new SelectList(courses, "Id", "Title");
+		}
+	}
+}
--- obj/Debug/net8.0/OnlineCourseManagementPortal.AssemblyInfo.cs
+++ obj/Debug/net8.0/OnlineCourseManagementPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineCourseManagementPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cd1c8c5d472eb700f12a4219e46eab5bcc19566")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd88c3b2505055fd4bd17a1a993039846c32069")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineCourseManagementPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineCourseManagementPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
